a536d8e00bf95d0be8c47170b4af37bda78c088d
Stephen <36123293+sparce@users.noreply.github.com>
2019-10-04 12:08:27 +1000

    Complete Q2

--- BOM_analysis.R
+++ BOM_analysis.R
@@ -36,8 +36,28 @@ stations_tidy <- BOM_stations %>%
 perth_data <- BOM_with_temps %>% 
   filter(Station_number == 9225)
 
-perth_data %>% 
-  ggplot(mapping = aes(x = t_min, y = t_max)) +
+q1_a <- perth_data %>% 
+  ggplot(mapping = aes(x = t_max, y = t_min)) +
   geom_point(alpha = 0.2)
 
-ggsave("figures/q1_a.png", width = 5, height = 5)
+q1_b <- perth_data %>% 
+  ggplot(mapping = aes(x = t_max, y = Rainfall)) +
+  geom_point(alpha = 0.2)
+
+q1_c <- perth_data %>% 
+  ggplot(mapping = aes(x = t_max, y = Solar_exposure)) +
+  geom_point(alpha = 0.2)
+
+ggsave("figures/q1_tmax_v_tmin.png", q1_a, width = 5, height = 5)
+ggsave("figures/q1_tmax_v_rainfall.png", q1_b, width = 5, height = 5)
+ggsave("figures/q1_tmax_v_solar.png", q1_c, width = 5, height = 5)
+
+## Question 2: -----
+# Display these four measurements for the Perth station in a single scatter plot by using 
+# additional aesthetic mappings.
+
+q2 <- perth_data %>% 
+  ggplot(mapping = aes(x = t_max, y = t_min, size = Rainfall, colour = Solar_exposure)) +
+  geom_point(alpha = 0.2)
+
+ggsave("figures/q2_all_measurements.png", q2, width = 5, height = 5)
